626db2fb421a0638add331e14dbe31ea74a363db
Md. Iqbal Khan <iqbalkhan319@gmail.com>
2025-10-28 22:50:51 +0600

    Update greeting message in welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,7 +39,7 @@
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-                    <h1>Hello from Jenkins automatic pipeline automatic trigger test</h1>
+                    <h1>Hello from Jenkins automatic pipeline automatic trigger test build</h1>
                 </div>
 
             </div>
